85ff66823ce38026f7df2b2d3352ab622d82551e
catcueto <catacueto@gmail.com>
2022-08-02 08:39:12 -0400

    completed Product.js in models folder

--- models/Category.js
+++ models/Category.js
@@ -4,24 +4,22 @@ const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
+// TODO: define columns
+// id: INT, no null values, primary key, auto incremenet
 Category.init(
   {
-    // TODO: define columns
-    // id: INT, no null values, set as primary key, auto increment)
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-
     // category_name: STRING, no null values
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
   },
-
   {
     sequelize,
     timestamps: false,
--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
 const sequelize = require("../config/connection");
+const Category = require("./Category");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,16 +11,21 @@ class Product extends Model {}
 Product.init(
   {
     // TODO: define columns
+    // id: INT, no null values, primary key, auto increment
     id: DataTypes.INTEGER,
     AllowNull: false,
     primaryKey: true,
     autoIncrement: true,
   },
+
   {
+    // product_name: string and no null values
     product_name: DataTypes.STRING,
     allowNull: false,
   },
+
   {
+    // price: decimal, no null values
     price: DataTypes.DECIMAL,
     allowNull: false,
     // validate that value is a decimal
@@ -27,14 +33,26 @@ Product.init(
       isDecimal: true,
     },
   },
+
   {
+    // stock: INT, no null values, default value : 10
     stock: DataTypes.INTEGER,
     allowNull: false,
-    defaultValue: 12,
+    defaultValue: 10,
+    // va;idate that value is numeric (INT)
     validate: {
       isInt: true,
     },
   },
+  {
+    // category_id: INT, references Category model's id
+    category_id: DataTypes.INTEGER,
+    references: {
+      model: Category,
+      key: "id",
+    },
+  },
+
   {
     sequelize,
     timestamps: false,
